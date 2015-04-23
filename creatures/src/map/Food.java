df1842c77b08445e1551e30102afaa8e220c22c8
viet_ba <m.vietba@gmail.com>
2015-04-23 22:31:41 +0200

    Done: - food power and weight properties

--- creatures/src/map/Food.java
+++ creatures/src/map/Food.java
@@ -33,4 +33,48 @@ public class Food {
 		this.foodID = resourceID;
 	}
 	
+	public int getPower() {
+		int power = 0;
+		
+		switch(this.foodID) {
+		case 0: //pizza
+			power = 50;
+			break;
+		case 1: //donut
+			power = 40;
+			break;
+		case 2: //grape
+			power = 20;
+			break;
+		case 3: //cabbage
+			power = 10;
+			break;
+		default:
+			break;
+		}
+		
+		return power;
+	} 
+	public int getWeight() {
+		int weight = 0;
+		
+		switch(this.foodID) {
+		case 0: //pizza
+			weight = 3;
+			break;
+		case 1: //donut
+			weight = 2;
+			break;
+		case 2: //grape
+			weight = 5;
+			break;
+		case 3: //cabbage
+			weight = 10;
+			break;
+		default:
+			break;
+		}
+		
+		return weight;
+	}
 }
